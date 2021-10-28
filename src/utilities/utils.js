63c5b3423d7da2289bcf3bb1b4634e7854796355
DESKTOP-BELTHOR\Belthor <beltranotero@hotmail.com>
2021-10-28 23:50:21 +0200

    creacion fichero utils

---
+++ src/utilities/utils.js
@@ -0,0 +1,10 @@
+
+
+export const findImage = (id,listaImagenes) => {
+    try {
+        return listaImagenes(`./${id}.jpg`).default
+
+    } catch (error) {
+        return ""; //por si no existe una imagen en recursos, para el id de producto, sin esto salta error
+    }
+}
